90b445e3cd5b6b54e98624aa5e51d415174b39d1
Beth Marshall <beth.marshall@askherconsulting.co.uk>
2025-08-14 16:23:02 +0100

    Day 4: exceptions, string manipulation, input output

---
+++ Ex08_Exceptions/src/day4/BadCalc.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class BadCalc {
+	
+	public int mult(int num1, int num2) {
+		
+		return num1 * num2;
+	}
+	
+	public int div(int num1, int num2) throws ArithmeticException {
+		return num1 / num2;
+	}
+}
